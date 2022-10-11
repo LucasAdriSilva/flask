0731c2dedcf2cb6dec558135ceb49f0816386bbf
Lucas Adriano <87877576+LucasAdriSilva@users.noreply.github.com>
2022-10-11 10:59:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
+
 
 @app.route('/')
 def index():
